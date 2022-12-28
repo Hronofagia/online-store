b28c3385e930bdafbbb49d56888820e4e02fd046
Nastassia <hronofaqia@gmail.com>
2022-12-28 21:57:15 +0200

    feat: create  progucts cards

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { footer } from './components/footer/footer';
 import { header } from './components/header/header';
 import Routing from './navigation/router';
 import { initRoutes } from './navigation/routes';
-
-import './styles.sass';
+import { CatalogItem } from './types';
 import { createHTML } from './utils/createHTML';
-
 import { LocalStorageUtil } from './utils/localStorage';
+import { showCards } from './pages/catalog/catalog';
+import './styles.sass';
 
 document.body.append(header);
 document.body.appendChild(createHTML('main', 'main'));
@@ -14,29 +14,7 @@ document.body.append(footer);
 initRoutes();
 Routing();
 
-// const store: { data: string[] } = {
-//   data: [],
-// };
-
-// const fetchData = async (): Promise<void> => {
-//   const data = await (await fetch('./data/categories.json')).json();
-//   console.log(data);
-//   const allData = await Promise.all(
-//     (data.categories as string[]).map(async (category) => {
-//       return await (await fetch(`./data/${category}.json`)).json();
-//     }),
-//   );
-//   console.log(allData);
-//   allData.forEach((element) => {
-//     store.data.push(...element.products);
-//   });
-
-//   console.log(store);
-// };
-
-// void fetchData();
-
-const CATALOG: object[] = [];
+export const store: CatalogItem[] = [];
 
 const fetchData = async (): Promise<void> => {
   const data = await (await fetch('./data/categories.json')).json();
@@ -46,14 +24,13 @@ const fetchData = async (): Promise<void> => {
     }),
   );
   allData.forEach((element) => {
-    CATALOG.push(...element);
+    store.push(...element);
   });
+  showCards();
 };
 
 void fetchData();
 
-console.log(CATALOG);
-
 const localStorageUtil = new LocalStorageUtil();
 localStorageUtil.pullProducts(2);
-console.log(localStorageUtil.getProducts());
+// console.log(localStorageUtil.getProducts());
